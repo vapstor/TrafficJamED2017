94e01da9d075d50584ef7960c055b452d855decd
Acauã Pitta <acaua.pitta@hotmail.com>
2017-09-25 05:28:17 -0300

    Gerador Atualizado - Erro STDlib

--- geradorValoresAleatorio/Gerador.h
+++ geradorValoresAleatorio/Gerador.h
@@ -7,23 +7,28 @@
 
 #ifndef GERADORVALORESALEATORIO_GERADOR_H_
 #define GERADORVALORESALEATORIO_GERADOR_H_
-
-#include <stdlib.h>
+//
+#include "stdlib.h>
+#include <stdio.h>
 
 namespace structures {
 
 template<typename T>
 	class Gerador {
 	public:
-		int Gerador(int lI, int lS) {
+		Gerador(int lI, int lS){
 			limiteInferior = lI;
 			limiteSuperior = lS;
+			num = numAleatorio(limiteInferior, limiteSuperior);
+		}
+
+		int numAleatorio(int li, int ls) {
 			srand((unsigned)time(NULL)); //numero aleatorio
 			return int(((double(rand())/RAND_MAX)*(limiteSuperior - limiteInferior)) +limiteInferior);
 		}
 
 	public:
-		int limiteInferior, limiteSuperior, numAleatorio;
+		int limiteInferior, limiteSuperior, num;
 	};
 }
 
